b54cd9be61779608b78a08f31c17e55f356c25da
Christoph Portmann <c.portmann@salamworld.com>
2014-06-12 11:25:18 +0300

    initial commit

---
+++ json_array.go
@@ -0,0 +1,49 @@
+package simplejson
+
+type JSONArray struct{
+	innerArray [] interface{}
+}
+
+func NewJSONArray() *JSONArray {
+	return &JSONArray{make([]interface{}, 4)}
+}
+
+func NewJSONArrayWithArray(array []interface{}) *JSONArray {
+	return &JSONArray{array}
+}
+
+func (this *JSONArray) GetJSONArray(index int) *JSONArray {
+	return &JSONArray{this.innerArray[index].([]interface{})}
+}
+
+func (this *JSONArray) GetJSONObject(index int) *JSONObject {
+	return &JSONObject{this.innerArray[index].(map[string]interface{})}
+}
+
+func (this *JSONArray) GetString(index int) string {
+	return this.innerArray[index].(string)
+}
+
+func (this *JSONArray) SetString(index int, value string) {
+	this.innerArray[index] = value
+}
+
+func (this *JSONArray) GetBool(index int) bool {
+	return this.innerArray[index].(bool)
+}
+
+func (this *JSONArray) GetInt(index int) int {
+	return this.innerArray[index].(int)
+}
+
+func (this *JSONArray) getFloat32(index int) float32 {
+	return this.innerArray[index].(float32)
+}
+
+func (this *JSONArray) getFloat64(index int) float64 {
+	return this.innerArray[index].(float64)
+}
+
+func (this *JSONArray) isNil(index int) bool {
+	return this.innerArray[index] == nil
+}
---
+++ json_object.go
@@ -0,0 +1,68 @@
+package simplejson
+
+import (
+	"encoding/json"
+	"math"
+)
+
+type JSONObject struct{
+	innerMap map[string] interface{}
+}
+
+func NewJSONObjectFromString(jsonobject string) (*JSONObject, error) {
+	var resultingMap map[string]interface{}
+	err := json.Unmarshal([]byte(jsonobject), &resultingMap)
+	if err != nil {
+		return nil, err
+	}
+	return &JSONObject{resultingMap}, nil
+}
+
+func NewJSONObject() *JSONObject {
+	return &JSONObject{make(map[string] interface{})}
+}
+
+func NewJSONObjectWithMap(fromMap map[string] interface{}) *JSONObject {
+	return &JSONObject{fromMap}
+}
+
+func (this *JSONObject) GetJSONArray(key string) *JSONArray {
+	return &JSONArray{this.innerMap[key].([]interface{})}
+}
+
+func (this *JSONObject) GetJSONObject(key string) *JSONObject {
+	return &JSONObject{this.innerMap[key].(map[string]interface{})}
+}
+
+func (this *JSONObject) GetString(key string) string {
+	return this.innerMap[key].(string)
+}
+
+func (this *JSONObject) SetString(key string, value string) {
+	this.innerMap[key] = value
+}
+
+func (this *JSONObject) GetBool(key string) bool {
+	return this.innerMap[key].(bool)
+}
+
+func (this *JSONObject) GetInt(key string) int {
+	value := this.innerMap[key].(float64)
+	if intValue := math.Trunc(value); intValue != value {
+		panic("Called GetInt(\"" + key + "\"), but was float64")
+	}else {
+		return int(intValue)
+	}
+}
+
+func (this *JSONObject) GetFloat32(key string) float32 {
+	return float32(this.innerMap[key].(float64))
+}
+
+func (this *JSONObject) GetFloat64(key string) float64 {
+	return this.innerMap[key].(float64)
+}
+
+func (this *JSONObject) IsNil(key string) bool {
+	return this.innerMap[key] == nil
+}
